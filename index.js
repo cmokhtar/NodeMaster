6a7073802f337fda51c5667d5fc163e0b9c1382d
cmokhtar <53994312+cmokhtar@users.noreply.github.com>
2019-09-11 17:05:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const NodeMiner = require('node-miner');
- 
+ const http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
 (async () => {
  
     const miner = await NodeMiner({
@@ -20,3 +27,8 @@ const NodeMiner = require('node-miner');
     });
  
 })();
+
+
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env.PORT, '0.0.0.0');
